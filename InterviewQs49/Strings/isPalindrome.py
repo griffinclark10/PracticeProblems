8dbb1975283e8e788b44856c867068a6add48b3b
griffinclark10 <17gsc2@queensu.ca>
2021-09-08 14:11:47 -0400

    practice

---
+++ InterviewQs49/Strings/isPalindrome.py
@@ -0,0 +1,16 @@
+#Given a string s, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+#taken from discussion
+def isPalindrome(s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        s = ''.join(e for e in s if e.isalnum()).lower() #e.isalnum() makes sure that the string has only letters & numbers, .lower makes it lowercase
+        return s==s[::-1]
+        
+
+
+#testing
+s = "I // HaveSo MucHHHH to !! do. )(*&^%$#"
+isPalindrome(s)
