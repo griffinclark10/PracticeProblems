2ef506893d4c6b912e80328cefabc0d8e3429c90
Griffin Clark <17gsc2@queensu.ca>
2021-11-10 12:03:56 -0500

    test

--- StockSell.py
+++ StockSell.py
@@ -1,2 +1,8 @@
 def stockSell(prices):
     return sum(prices[i] - prices[i-1] if prices[i] > prices[i-1] else 0 for i in range(1, len(prices)))
+
+
+prices = [2,5,4,8,6,3,1,4,7]
+answer = 13
+
+print(stockSell(prices) == answer)
