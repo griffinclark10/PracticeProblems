4c60347f4f07a3b0ca3aef29fdad946a101996fc
Griffin Clark <17gsc2@queensu.ca>
2021-11-23 11:49:45 -0500

    not by myself :(

--- LeetcodeDaily/longestCommonPrefix.py
+++ LeetcodeDaily/longestCommonPrefix.py
@@ -1,8 +1,21 @@
-def longestCommonPrefix(words):
-    for i in words:
-        for j in range(len(i)):
-            pass
+def longestCommonPrefix(strs):
+    # first = words[0]
+    # start = first[0]
+    # prefix = ""
+    # for j in range(len(first)):
+    #     prefix += first[j]
+    #     for i in range(1, len(words)):
+    #         print(words[i])
+    #         if words[i].startswith(start) != True:
+    #             return ""
+            
+    # return prefix    
+    strs.sort()
+    prefix = strs[0]
+    while prefix != strs[-1][:len(prefix)]:
+        prefix = prefix[:-1]
+    return prefix    
 
 words = ["flower","flow","flight"]
-longestCommonPrefix(words)
-        
+a = longestCommonPrefix(words)
+print(a)
