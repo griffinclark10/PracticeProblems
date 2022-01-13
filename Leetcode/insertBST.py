ecffce05aa0060f4f5d32940d275f4665b7bc3f7
Griffin Clark <17gsc2@queensu.ca>
2022-01-13 18:49:11 -0500

    i suck

--- Leetcode/insertBST.py
+++ Leetcode/insertBST.py
@@ -1,2 +1,9 @@
+#parent = n
+#left node = 2n+1
+#right node = 2n+2
+
 def insertIntoBST(root: Optional[TreeNode], val: int)->Optional[TreeNode]:
-    print("j")
+    if root is None: return TreeNode(val) 
+    if root.val > val:  root.left = insertIntoBST(root.left, val)
+    else: root.right = insertIntoBST(root.right, val)
+    return root
